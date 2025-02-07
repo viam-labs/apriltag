35680524155b2e2f369cf109121bf63c006980ba
Ray Bjorkman <raymond@viam.com>
2025-02-07 11:36:05 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,86 @@
+import asyncio
+from typing import (Any, ClassVar, Dict, Final, List, Mapping, Optional,
+                    Sequence)
+
+from typing_extensions import Self
+from viam.components.pose_tracker import *
+from viam.module.module import Module
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import Geometry, PoseInFrame, ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.easy_resource import EasyResource
+from viam.resource.types import Model, ModelFamily
+from viam.utils import ValueTypes
+
+
+class Apriltag(PoseTracker, EasyResource):
+    MODEL: ClassVar[Model] = Model(ModelFamily("luddite", "apriltag"), "apriltag")
+
+    @classmethod
+    def new(
+        cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ) -> Self:
+        """This method creates a new instance of this PoseTracker component.
+        The default implementation sets the name from the `config` parameter and then calls `reconfigure`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+            dependencies (Mapping[ResourceName, ResourceBase]): The dependencies (both implicit and explicit)
+
+        Returns:
+            Self: The resource
+        """
+        return super().new(config, dependencies)
+
+    @classmethod
+    def validate_config(cls, config: ComponentConfig) -> Sequence[str]:
+        """This method allows you to validate the configuration object received from the machine,
+        as well as to return any implicit dependencies based on that `config`.
+
+        Args:
+            config (ComponentConfig): The configuration for this resource
+
+        Returns:
+            Sequence[str]: A list of implicit dependencies
+        """
+        return []
+
+    def reconfigure(
+        self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]
+    ):
+        """This method allows you to dynamically update your service when it receives a new `config` object.
+
+        Args:
+            config (ComponentConfig): The new configuration
+            dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
+        """
+        return super().reconfigure(config, dependencies)
+
+    async def get_poses(
+        self,
+        body_names: List[str],
+        *,
+        extra: Optional[Mapping[str, Any]] = None,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Dict[str, PoseInFrame]:
+        raise NotImplementedError()
+
+    async def do_command(
+        self,
+        command: Mapping[str, ValueTypes],
+        *,
+        timeout: Optional[float] = None,
+        **kwargs
+    ) -> Mapping[str, ValueTypes]:
+        raise NotImplementedError()
+
+    async def get_geometries(
+        self, *, extra: Optional[Dict[str, Any]] = None, timeout: Optional[float] = None
+    ) -> List[Geometry]:
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    asyncio.run(Module.run_from_registry())
+
